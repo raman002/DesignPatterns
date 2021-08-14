4e01e737d7fc7e4ce317259e9f67da6018124283
Rewatiraman Singh <singhraman834@gmail.com>
2021-08-14 23:07:18 +0530

    Added singleton design pattern

---
+++ src/main/java/creational/singleton/MyDataSource.java
@@ -0,0 +1,2 @@
+package creational.singleton;public class MyDataSource {
+}
---
+++ src/main/java/creational/singleton/SingletonPattern.java
@@ -0,0 +1,7 @@
+package creational.singleton;
+
+public class SingletonPattern {
+    public static void main(String[] args) {
+
+    }
+}
