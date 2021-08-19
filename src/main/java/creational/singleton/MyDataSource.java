4e3ecd1a53dc3c8f0eec81a1be6cb4b9df89934f
Rewatiraman Singh <singhraman834@gmail.com>
2021-08-19 11:33:46 -0700

    Added code for the singleton design pattern

--- src/main/java/creational/singleton/MyDataSource.java
+++ src/main/java/creational/singleton/MyDataSource.java
@@ -1,2 +1,28 @@
-package creational.singleton;public class MyDataSource {
+package creational.singleton;
+
+public class MyDataSource {
+    private static MyDataSource dataSource;
+
+    private MyDataSource() {}
+
+    public static MyDataSource getInstance() {
+        if (dataSource == null) {
+            dataSource = new MyDataSource();
+        }
+        return dataSource;
+    }
+
+    /* If multiple threads try to access the getInstance() method at once then make
+    * the method as synchronized or write the instance creation logic inside
+    * synchronized block*/
+
+    public static MyDataSource getThreadSafeInstance() {
+        synchronized (dataSource) {
+            if (dataSource == null) {
+                dataSource = new MyDataSource();
+            }
+        }
+
+        return dataSource;
+    }
 }
--- src/main/java/creational/singleton/SingletonPattern.java
+++ src/main/java/creational/singleton/SingletonPattern.java
@@ -2,6 +2,10 @@ package creational.singleton;
 
 public class SingletonPattern {
     public static void main(String[] args) {
+        MyDataSource instance1 = MyDataSource.getInstance();
+        MyDataSource instance2 = MyDataSource.getInstance();
 
+        System.out.println("instance1 = " + instance1);
+        System.out.println("instance2 = " + instance2);
     }
 }
