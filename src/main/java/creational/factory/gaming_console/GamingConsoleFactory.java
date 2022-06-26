e762755f5d20438c51a98d9f06c29445ad98622c
Rewatiraman Singh <singhraman834@gmail.com>
2022-06-26 21:12:30 +0530

    Added code for the factory design pattern

--- src/main/java/creational/factory/gaming_console/GamingConsoleFactory.java
+++ src/main/java/creational/factory/gaming_console/GamingConsoleFactory.java
@@ -3,12 +3,10 @@ package creational.factory.gaming_console;
 public class GamingConsoleFactory {
 
     public static Console buildConsole(IGamingConsole.ConsoleType consoleType) {
-        switch(consoleType) {
+        return switch(consoleType) {
             case NINTENDO -> new NintendoConsole().buildConsole();
             case XBOX -> new Xbox().buildConsole();
             case PLAYSTATION -> new PlayStation().buildConsole();
         };
-
-        return null;
     }
 }
